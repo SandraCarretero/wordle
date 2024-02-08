0210750dac276180b05461347b202392ef07fc20
SandraCarretero <sandracarretero24@gmail.com>
2024-02-08 09:07:12 +0100

    create wordle

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,1 +1,6 @@
 // Aquí van los getElementById y los querySelectorAll que pondrías al principio.
+const gameElement = document.getElementById('game');
+const formElement = document.getElementById('form');
+const popupElement = document.getElementById('pop-up');
+
+export {gameElement, formElement, popupElement}
